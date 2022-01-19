3ac1903abf8c9bd37d1aac6131053e0624b61591
jeffreyignaciowong <jeffreyignaciowong@gmail.com>
2022-01-18 21:07:32 -0500

    change file tree

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ app.use('/inventory', inventoryRouter);
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('../front_end/build'));
+    app.use(express.static('./front_end/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'front_end', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'front_end', 'build', 'index.html'))
     });
 }
 
